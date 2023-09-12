44081310ad70966456011dcf0d574a526af23a8f
Medo20300 <medhatconcrete@gmail.com>
2023-09-12 03:29:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char g = 'a';
+
+	while (g <= 'z')
+	{
+		_putchar(g);
+		g++;
+
+	}
+	_putchar('\n');
+
+}
