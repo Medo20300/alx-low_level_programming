4da9c0bd3b21ab1bd78b4997a99aff077d595361
Medo20300 <medhatconcrete@gmail.com>
2023-12-22 13:20:30 -0800

    2050

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return a +b;
+}
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+int num1(int a, int b)
+{
+	return a * b;
+}
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error : DIvision by zero\n");
+		return 0;
+	}
+	return a /b;
+}
+int mod(int a, int b)
+{
+	if (b ==0)
+	{
+		printf("Error :Division by zero\n");
+		return 0;
+	}
+	return a %b;
+}
